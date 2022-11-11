6bdd7b1b749fafb6595bb9e1028bd4215c3d5400
ic__e <yoshidashi058@gmail.com>
2022-11-11 17:37:24 +0900

    add: add author component

---
+++ components/Author.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+type Author = {
+  author: any;
+};
+
+const Author = ({ author }: Author) => {
+  return (
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          src={author.photo.url}
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="aligne-middle rounded-full"
+          unoptimized
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
+    </div>
+  );
+};
+
+export default Author;

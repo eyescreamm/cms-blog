afc8382f8aace1843ead747f06fbb40dd776b399
ic__e <yoshidashi058@gmail.com>
2022-11-13 16:57:23 +0900

    docs: common window title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,14 @@
 import React, { Children, ReactNode } from "react";
 import { Header } from "./";
+import Head from "next/head";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
+      <Head>
+        <title>ic_e blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       {children}
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
@@ -7,10 +6,6 @@ import { getPosts } from "../services";
 const Home: NextPage = ({ posts }: any) => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post: any, index: any) => (

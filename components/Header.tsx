ba4b2874f7a40d4845ce0d11ab5f4a66044a5e36
ic__e <yoshidashi058@gmail.com>
2022-11-13 16:52:53 +0900

    docs: change blog title

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              GraphCMS
+              ic_e blog
             </span>
           </Link>
         </div>
